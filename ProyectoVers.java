e48aa67d0d232b85cf002477b3d6d7ce7393beeb
RamonYMarcos <ramonfpb02@gmail.com>
2024-02-05 13:05:36 +0100

    Vaina seria

--- ProyectoVers.java
+++ ProyectoVers.java
@@ -1,10 +1,12 @@
 package control_version;
 
+import javax.swing.JOptionPane;
+
 public class ProyectoVers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JOptionPane.showMessageDialog(null, "Eta vaina e seria");
+		JOptionPane.showMessageDialog(null, "heheheh");
 	}
 
 }
